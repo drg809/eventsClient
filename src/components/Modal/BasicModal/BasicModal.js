2579b8e6be396185421091e618155cdafb8bdc7d
Daniel Rodriguez <drg809@gmail.com>
2021-06-19 00:23:03 +0200

    basic modal functions and control

--- src/components/Modal/BasicModal/BasicModal.js
+++ src/components/Modal/BasicModal/BasicModal.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import LogoWhiteEvents from '../../../assets/png/logo-white.png';
 
-export default function BasicModal() {
+import './BasicModal.scss';
+
+
+
+export default function BasicModal(props) {
+   const { show, setShow, children } = props;
+
    return (
-      <div>
-         <h1>Basic Modal...</h1>
-      </div>
+      <Modal
+         className="basic-modal"
+         show={show}
+         onHide={() => setShow(false)}
+         centered
+         size="lg"
+      >
+         <Modal.Header>
+            <Modal.Title>
+               <img src={LogoWhiteEvents} alt="events" />
+            </Modal.Title>
+         </Modal.Header>
+         <Modal.Body>{children}</Modal.Body>
+      </Modal>
    )
 }
---
+++ src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Row, Col, Form, Button, Spinner } from 'react-bootstrap';
+
+import './SignUpForm.scss';
+
+export default function SignUpForm(props) {
+   const { setShowModal } = props;
+
+   const onSubmit = e => {
+      e.preventDefault();
+      setShowModal(false);
+   }
+
+   return (
+      <div className='sign-up-form'>
+         <h2>Crea tu cuenta</h2>
+         <Form onSubmit={onSubmit}>
+            <Form.Group>
+               <Row>
+                  <Col>
+                     <Form.Control type='text' placeholder='Nombre' />
+                  </Col>
+                  <Col>
+                     <Form.Control type='text' placeholder='Apellidos' />
+                  </Col>
+               </Row>
+            </Form.Group>
+            <Form.Group>
+               <Form.Control type='email' placeholder='Correo electrónico' />
+            </Form.Group>
+            <Form.Group>
+               <Row>
+                  <Col>
+                     <Form.Control type='password' placeholder='Contraseña' />
+                  </Col>
+                  <Col>
+                     <Form.Control type='password' placeholder='Repetir contraseña' />
+                  </Col>
+               </Row>
+            </Form.Group>
+
+            <Button variant='primary' type='submit'>
+               Registrarse
+            </Button>
+         </Form>
+      </div>
+   )
+}
