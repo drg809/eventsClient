80c459319ee36c5677513871bd97c64c68387b14
Daniel Rodriguez <drg809@gmail.com>
2021-06-19 00:23:29 +0200

    call to basic modal when click and control

--- src/page/SignInSingUp/SignInSingUp.js
+++ src/page/SignInSingUp/SignInSingUp.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faUsers, faComment } from '@fortawesome/free-solid-svg-icons';
+
+import BasicModal from '../../components/Modal/BasicModal/BasicModal';
+import SignUpForm from '../../components/SignUpForm';
 import LogoWhiteEvents from '../../assets/png/logo-white.png';
 import LogoEvents from '../../assets/png/logo.png';
 
 import './SignInSingUp.scss';
 
 export default function SignInSingUp() {
+   const [showModal, setShowModal] = useState(false);
+   const [contentModal, setContentModal] = useState(null);
+
+   const openModal = content => {
+      setShowModal(true);
+      setContentModal(content);
+   }
+
    return (
-      <Container className="signin-signup" fluid>
-         <Row>
-            <LeftComponent />
-            <RightComponent />
-         </Row>
-      </Container>
+      <>
+         <Container className="signin-signup" fluid>
+            <Row>
+               <LeftComponent />
+               <RightComponent openModal={openModal} setShowModal={setShowModal} />
+            </Row>
+         </Container>
+         <BasicModal show={showModal} setShow={setShowModal}>
+            {contentModal}
+         </BasicModal>
+      </>
    )
 }
 
@@ -41,17 +57,19 @@ function LeftComponent() {
    )
 }
 
-function RightComponent() {
+function RightComponent(props) {
+   const { openModal, setShowModal } = props;
+
    return (
       <Col className="signin-signup__right">
          <div>
             <img src={LogoWhiteEvents} alt="events"/>
             <h2>Mira las actividades de Don Bosco.</h2>
             <h3>Únete a la comunidad de Don Bosco hoy mismo.</h3>
-            <Button variant="primary">
+            <Button onClick={ () => openModal(<SignUpForm setShowModal={setShowModal} />) } variant="primary">
                Regístrate
             </Button>
-            <Button variant="outline-primary">
+            <Button onClick={ () => openModal(<h2>Formulario de login</h2>) } variant="outline-primary">
                Iniciar sesion
             </Button>
          </div>
