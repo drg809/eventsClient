fb2353219b5931362c53548582c10aee0dfcae35
Daniel Rodriguez <drg809@gmail.com>
2021-06-19 01:14:26 +0200

    toast system load

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import SignInSingUp from './page/SignInSingUp'
+import { ToastContainer } from 'react-toastify';
+
 
 export default function App() {
   const [user, setUser] = useState({name:"Dani"});
 
-  return <div>{ user ? <SignInSingUp /> : <h1>No estas logeado</h1> }</div>;
+  return (
+    <div>
+      { user ? <SignInSingUp /> : <h1>No estas logeado</h1> }
+      <ToastContainer position='top-right' autoClose={5000} hideProgressBar newestOnTop={false} closeOnClick rtl={false} pauseOnVisibilityChange />
+    </div>
+  );
+
 }
