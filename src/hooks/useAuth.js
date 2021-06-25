f37f843d025abad0b8d5525fcadea55cb8308ab8
Daniel Rodriguez <drg809@gmail.com>
2021-06-25 21:56:54 +0200

    hooks system to grab authcontext

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,5 @@
+import { useContext } from 'react';
+import { AuthContext } from '../utils/context';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => useContext(AuthContext);
