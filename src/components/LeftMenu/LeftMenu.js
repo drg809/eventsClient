db2555f7acd088895eadb2a46d0c43378e79bd68
Daniel Rodriguez <drg809@gmail.com>
2021-06-25 22:01:01 +0200

    route to user with id

--- src/components/LeftMenu/LeftMenu.js
+++ src/components/LeftMenu/LeftMenu.js
@@ -5,12 +5,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faUsers, faPowerOff } from '@fortawesome/free-solid-svg-icons';
 
 import { logoutApi } from '../../api/auth';
+import useAuth from '../../hooks/useAuth';
 import LogoWhite from '../../assets/png/logo-white.png';
 
 import './LeftMenu.scss';
 
 export default function LeftMenu(props) {
    const { setRefreshCheckLogin } = props;
+   const user = useAuth();
 
    const logout = () => {
       logoutApi();
@@ -23,7 +25,7 @@ export default function LeftMenu(props) {
 
          <Link to="/"> <FontAwesomeIcon icon={faHome} /> Inicio</Link>
          <Link to="/users"> <FontAwesomeIcon icon={faUsers} /> Usuarios</Link>
-         <Link to="/profile"> <FontAwesomeIcon icon={faUser} /> Perfíl</Link>
+         <Link to={`/user/${user?._id}`}> <FontAwesomeIcon icon={faUser} /> Perfíl</Link>
          <Link to="" onClick={logout}> <FontAwesomeIcon icon={faPowerOff} /> Cerrar sesión</Link>
 
          <Button>Crear evento</Button>
