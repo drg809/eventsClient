127eab1064e8591c459fbb8425d5415456af5de8
Daniel Rodriguez <drg809@gmail.com>
2021-06-25 23:10:02 +0200

    display avatar and avatarNotFound image

--- src/components/User/BannerAvatar/BannerAvatar.js
+++ src/components/User/BannerAvatar/BannerAvatar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import { API_HOST } from '../../../utils/constant'
+import AvatarNotFound from '../../../assets/png/avatar-no-found.png';
+import { API_HOST } from '../../../utils/constant';
 import './BannerAvatar.scss';
 
 export default function BannerAvatar(props) {
    const { user } = props;
-   const bannerUrl =  user?.banner ? `${API_HOST}/users/banner?id=${user.id}` : null;
+   const bannerUrl = user?.banner ? `${API_HOST}/users/banner?id=${user.id}` : null;
+   const avatarUrl = user?.avatar ? `${API_HOST}/users/avatar?id=${user.id}` : AvatarNotFound;
 
    return (
       <div className="banner-avatar" style={{ backgroundImage: `url(${bannerUrl})` }}>
-         <div className="avatar"></div>
+         <div className="avatar" style={{ backgroundImage: `url(${avatarUrl})` }} />
       </div>
    )
 }
