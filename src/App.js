b3b2645dc1a66d1566e0f6ae84a50ff7c779feb4
Daniel Rodriguez <drg809@gmail.com>
2021-06-25 20:40:35 +0200

    home page and error 404 page load

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SignInSingUp from './page/SignInSingUp'
 import { ToastContainer } from 'react-toastify';
 import { AuthContext } from './utils/context';
+import { isUserLogged } from './api/auth';
+import Routing from './routers/Routing';
 
 
 export default function App() {
   const [user, setUser] = useState(null);
+  const [loadUser, setLoadUser] = useState(false);
+  const [refreshCheckLogin, setRefreshCheckLogin] = useState(false)
+
+  useEffect(() => {
+    setUser(isUserLogged());
+    setRefreshCheckLogin(false);
+    setLoadUser(true);
+  }, [refreshCheckLogin]);
+
+  if(!loadUser) return null;
 
   return (
     <AuthContext.Provider value={user}>
-      { user ? <h1>Estas logeado</h1>  : <SignInSingUp /> }
+      { user ? <Routing />  : <SignInSingUp setRefreshCheckLogin={setRefreshCheckLogin} /> }
       <ToastContainer position='top-right' autoClose={5000} hideProgressBar newestOnTop={false} closeOnClick rtl={false} pauseOnVisibilityChange />
     </AuthContext.Provider>
   );
---
+++ src/page/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Home.scss';
+
+export default function Home() {
+   return (
+      <div>
+         <h2>Estamos en home</h2>
+      </div>
+   )
+}
