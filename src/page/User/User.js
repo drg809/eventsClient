ec38fba646e5b3462eb947c61e8bb208d684aca0
Daniel Rodriguez <drg809@gmail.com>
2021-06-25 22:07:37 +0200

    user pages basic structure

--- src/page/User/User.js
+++ src/page/User/User.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Button, Spinner } from 'react-bootstrap';
+
+import BasicLayout from '../../layout/BasicLayout/BasicLayout'
 import './User.scss';
 
 export default function User() {
    return (
-      <div>
-         <h2>User...</h2>
-      </div>
+      <BasicLayout className="user">
+         <div className="user__title">
+            <h2>Usuario</h2>
+         </div>
+         <div>Banner Usuario</div>
+         <div>Info usuario</div>
+         <div className="user__events">Lista de eventos</div>
+      </BasicLayout>
    );
 }
