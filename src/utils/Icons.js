491bc4ef1772da6c2c305a1c5edcec6463d55cd3
Daniel Rodriguez <drg809@gmail.com>
2021-06-26 03:11:32 +0200

    camera svg upload

--- src/utils/Icons.js
+++ src/utils/Icons.js
@@ -2,3 +2,4 @@ export { ReactComponent as LocationIcon } from '../assets/svg/location.svg';
 export { ReactComponent as LinkIcon } from '../assets/svg/link.svg';
 export { ReactComponent as BirthIcon } from '../assets/svg/date-birth.svg';
 export { ReactComponent as CloseIcon } from '../assets/svg/close.svg';
+export { ReactComponent as CameraIcon } from '../assets/svg/camara.svg';
