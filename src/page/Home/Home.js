1d4a48a513f0d337d1d2bb3264a38c1926e7c084
Daniel Rodriguez <drg809@gmail.com>
2021-07-03 05:12:27 +0200

    home page with header

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Spinner } from 'react-bootstrap';
+import { Button, Spinner, ButtonGroup } from 'react-bootstrap';
+import queryString from 'query-string';
+import { isEmpty } from 'lodash';
+import { useDebouncedCallback } from 'use-debounce';
+import { withRouter } from 'react-router-dom';
+
 
 import BasicLayout from '../../layout/BasicLayout/BasicLayout';
 import ListEvents from '../../components/Events/ListEvents';
@@ -7,23 +12,44 @@ import { getEventsFollowerApi } from '../../api/event';
 
 import './Home.scss';
 
-export default function Home(props) {
-   const { setRefreshCheckLogin } = props;
+function Home(props) {
+   const { setRefreshCheckLogin, location, history } = props;
    const [events, setEvents] = useState(null);
    const [page, setPage] = useState(1);
    const [loadingEvents, setLoadingEvents] = useState(false);
+   const params = useUsersQuery(location);
+   const [typeUser, setTypeUser] = useState(params.type || 'follow');
+
+   const onSearch = useDebouncedCallback((value) => {
+      setEvents(null);
+      history.push({search: queryString.stringify({...params, search: value, page: 1 }),});
+   }, 200);
+
 
    useEffect(() => {
-      getEventsFollowerApi(page).then(response => {
-         setEvents(formatModel(response));
-         !events && response ? setEvents(formatModel(response)) : (
-            !response ? setLoadingEvents(0) : (
-               setEvents([...events, ...formatModel(response)]) && setLoadingEvents(false)
-            )
-         )
-      }).catch(() => {});
+      getEventsFollowerApi(queryString.stringify(params)).then(response => {
+         // eslint-disable-next-line eqeqeq
+         params.page == 1 ? (isEmpty(response) ? setEvents([]) : setEvents(formatModel(response))) : (!response ? setLoadingEvents(0) :  setEvents([...events, ...formatModel(response)]));
+         setLoadingEvents(false);
+      }).catch(() => {
+         setEvents([]);
+      });
    // eslint-disable-next-line react-hooks/exhaustive-deps
-   }, [page]);
+   }, [location]);
+
+   const onChangeType = type => {
+      setEvents(null);
+      if(type === 'new') {
+         setTypeUser('new');
+      } else {
+         setTypeUser('follow');
+      }
+
+      history.push({
+         search: queryString.stringify({type: type, page: 1, search: ''})
+      })
+   }
+
 
    const moreData = () => {
       setLoadingEvents(true);
@@ -33,8 +59,14 @@ export default function Home(props) {
    return (
       <BasicLayout className='home' setRefreshCheckLogin={setRefreshCheckLogin} >
          <div className='home__title'>
-            <h2>Inicio</h2>
+            <h2>Eventos Próximos</h2>
+            <input type='text' placeholder='Busca un evento...' onChange={(e) => onSearch(e.target.value)} />
          </div>
+
+         <ButtonGroup className='users__options'>
+            <Button className={typeUser === 'follow' && 'active'} onClick={() => onChangeType('follow')}>Siguiendo</Button>
+            <Button className={typeUser === 'new' && 'active'} onClick={() => onChangeType('new')}>Nuevos</Button>
+         </ButtonGroup>
          {events && <ListEvents events={events} /> }
          <Button onClick={moreData} className='load-more'>
             {!loadingEvents ? (
@@ -61,3 +93,11 @@ function formatModel(events) {
    });
    return eventsTemp;
 }
+
+function useUsersQuery(location) {
+   const { page = 1, type = 'follow', search } = queryString.parse(location.search);
+
+   return {page, type, search};
+}
+
+export default withRouter(Home);
