9d477b8e766430de9f9a9a1f0f5a22c1d27dd84f
Daniel Rodriguez <drg809@gmail.com>
2021-07-03 04:02:17 +0200

    error page config

--- src/page/Error404/Error404.js
+++ src/page/Error404/Error404.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Error404Image from '../../assets/png/error-404.png';
+import Logo from '../../assets/png/logo.png';
 
 import './Error404.scss';
 
 export default function Error404() {
    return (
-      <div>
-         <h2>Error 404</h2>
+      <div className='error404'>
+         <img src={Logo} alt='eventos' />
+         <img src={Error404Image} alt='Error404' />
+         <Link to='/'>Volver al Inicio</Link>
       </div>
    );
 }
